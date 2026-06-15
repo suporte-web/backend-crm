14668f37315375a2c4184b35f309e258865c1a96
Caroline Augusto <126285992+CarolAugust@users.noreply.github.com>
2026-06-15 10:55:04 -0300

    Ajustando para subir para a VM

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,6 +3,8 @@ import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
 
+
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
